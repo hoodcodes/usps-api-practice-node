4c40cf18613e9e80f1acfb7653eaf533bfdf742f
Christopher Hood <chris@cshood.com>
2022-02-12 16:09:41 -0700

    add dependencies and update index

---
+++ index.js
@@ -0,0 +1,8 @@
+const PORT = 7000;
+const axios = require("axios");
+const axios = require("cheerio");
+const axios = require("express");
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server running on PORT $ {PORT}`));
