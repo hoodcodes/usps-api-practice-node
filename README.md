dd2528463d34cb3de55fcea09a0b768025609c3e
Christopher Hood <chris@cshood.com>
2022-02-21 07:43:56 -0700

    add usps-web-tools-sdk for review of xml encoding. add .env file to separate out secrets

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-#  USPS API Practice - for Node
+# USPS API Practice - for Node
 
+### Notes
+
+- this api will accept a json address object, which i will parse, creating xml for the uspa post request to do address validation
+
+- i will send it back as json as well, including:
+  - if error occurred - error information
+  - if any data changed - pass back a dirty / changed flag
--- index.js
+++ index.js
@@ -1,8 +1,144 @@
-const PORT = 7000;
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const PORT = process.env.PORT;
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
+const utf8 = require("utf-8");
+const parseString = require("xml2js").parseString;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`server running on PORT $ {PORT}`));
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+
+var usps = require("usps-web-tools-node-sdk");
+
+console.log(process.env.USERNAME);
+
+// tell it to use your username from the e-mail
+usps.configure({ userID: process.env.USERNAME });
+
+// invoke the API you need with...
+usps.addressInformation.cityStateLookup(
+  // a data object with the required fields
+  {
+    zipCode: ["90210"],
+  },
+  // and a callback
+  function (error, response) {
+    if (error) {
+      // if there's a problem, the error object won't be null
+      console.log(error);
+    } else {
+      // otherwise, you'll get a response object
+      console.log(JSON.stringify(response));
+    }
+  }
+);
+
+// invoke the API you need with...
+usps.addressInformation.verify(
+  // a data object with the required fields
+  {
+    includeOptionalElements: true,
+    returnCarrierRoute: true,
+    address: [
+      //examples
+      // { address2: "205 bagwell ave", city: "nutter fort", state: "wv" },
+      // {
+      //   firmName: "mrs. jane doe",
+      //   address1: "apt. 1",
+      //   address2: "150 calle a",
+      //   city: "san juan",
+      //   state: "pr",
+      //   urbanization: "urb las gladiolas",
+      //   zip5: "00926",
+      //   zip4: "3232",
+      // },
+      {
+        address1: "7000 w morning dove",
+        address2: "",
+        city: "glendale",
+        state: "az",
+        zip5: "85308",
+        zip4: "",
+      },
+    ],
+  },
+  // and a callback
+  function (error, response) {
+    if (error) {
+      // if there's a problem, the error object won't be null
+      console.log(error);
+    } else {
+      // otherwise, you'll get a response object
+      console.log(JSON.stringify(response));
+    }
+  }
+);
+
+const xmlRequest =
+  '<AddressValidateRequest USERID="710THECO0597"><Revision>1</Revision><Address ID="0"><Address1>7000 West Morning Dove Dr</Address1><Address2></Address2><City>Glendale</City><State>Arizona</State><Zip5>85308</Zip5><Zip4></Zip4></Address></AddressValidateRequest>';
+
+const options = {
+  method: "post",
+  url: "http://production.shippingapis.com/ShippingApi.dll",
+  data: {
+    API: "Verify",
+    XML: xmlRequest,
+  },
+  headers: {
+    "Content-Type": "application/xml",
+  },
+};
+
+// axios(options)
+//   .then((response) => {
+//     console.log(response.data);
+//     parseString(response.data, (err, result) => {
+//       //console.log(result)
+//       //replace below with xml parsing
+//       // console.log(JSON.stringify(result['soap:Envelope']['soap:Body']))
+//       // var searchResult = result['soap:Envelope']['soap:Body'][0]['QASearchResult'][0]['QAPicklist'][0]['PicklistEntry']
+//       // searchResult.forEach((item) => {
+//       //   console.dir(item)
+//       // })
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+/*
+  <Error><Number>80040B19</Number><Description>XML Syntax Error: 
+  Please check the XML request to see if it can be parsed.
+  </Description><Source>USPSCOM::DoAuth</Source></Error>
+  */
+
+/*
+
+
+
+
+
+
+*/
+
+// request.post({
+//     url:"http://production.shippingapis.com/ShippingApi.dll",
+//     port: 9000,
+//     method:"POST",
+//     headers:{
+//         'Content-Type': 'application/xml',
+//     },
+//     data:{
+//       api: 'Verify',
+//       XML: xmlRequest
+//     }
+// },
+// function(error, response, body){
+//     console.log(response.statusCode);
+//     console.log(body);
+//     console.log(error);
+// });
